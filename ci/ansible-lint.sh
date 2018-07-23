8438d80dce51cb1dbbc1931f390a73b31434d24d
Jan Michalek <jan.michalek@betsys.com>
2018-07-23 13:13:25 +0200

    Updated linter image

--- ci/ansible-lint.sh
+++ ci/ansible-lint.sh
@@ -3,7 +3,7 @@
 
 . "$(dirname "${BASH_SOURCE[0]}")/lib.sh" || exit 1
 
-ANSIBLE_LINTER_IMAGE=registry.betsys.com/org/ansible-linter:16185-7b55f6a340d9d126ab2d58cb146d5d86674543dd-master
+ANSIBLE_LINTER_IMAGE=registry.betsys.com/org/ansible-linter:latest-master
 
 echo_exec docker run \
     -t \
