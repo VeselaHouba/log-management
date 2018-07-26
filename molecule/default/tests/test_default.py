523b3701b5f52a89ee1d1a1684fe649a492398a7
Jan Michalek <jan.michalek@betsys.com>
2018-07-26 16:29:34 +0200

    cerebro implementation

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -22,3 +22,13 @@ def test_port_open(host):
 def test_elastalert_running(host):
     cmd = host.run("systemctl status elastalert")
     assert cmd.rc == 0
+
+
+def test_cerebro_running(host):
+    cmd = host.run("systemctl status cerebro")
+    assert cmd.rc == 0
+
+
+def test_cerebro_port_open(host):
+    sock = host.socket("tcp://0.0.0.0:9000")
+    assert sock.is_listening
