1e431aec74218fe1dedf1c10a5298aaaa0cacd12
michalekj <jan.michalek@betsys.com>
2019-04-15 12:00:14 +0200

    [molecule] Temporarily remove elastalert
    Until https://github.com/Yelp/elastalert/issues/2204 is fixed

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -11,9 +11,9 @@ def test_port_open(host):
     assert sock.is_listening
 
 
-def test_elastalert_running(host):
-    cmd = host.run("systemctl status elastalert")
-    assert cmd.rc == 0
+# def test_elastalert_running(host):
+#     cmd = host.run("systemctl status elastalert")
+#     assert cmd.rc == 0
 
 
 def test_cerebro_running(host):
