e027452a46c03eccb0389edbe6ec6c0953f9acc5
michalekj <jan.michalek@betsys.com>
2019-04-15 08:03:02 +0200

    Changes for version 7.x of ELK stack

---
+++ molecule/7.0/tests/test_default.py
@@ -0,0 +1,31 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_port_open(host):
+    sock = host.socket("tcp://0.0.0.0:9200")
+    assert sock.is_listening
+
+
+def test_elastalert_running(host):
+    cmd = host.run("systemctl status elastalert")
+    assert cmd.rc == 0
+
+
+def test_cerebro_running(host):
+    cmd = host.run("systemctl status cerebro")
+    assert cmd.rc == 0
+
+
+def test_cerebro_port_open(host):
+    sock = host.socket("tcp://0.0.0.0:9000")
+    assert sock.is_listening
+
+
+def test_rsyslog_service(host):
+    assert host.service('rsyslog').is_enabled
+    assert host.service('rsyslog').is_running
